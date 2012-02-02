d18e39982b1c41eb4d9696f563611ae1b3353635
Daniele Trapani <daniele.trapani@live.it>
2012-02-02 02:05:02 +0100

    new options

--- phpbase64.rb
+++ phpbase64.rb
@@ -1,6 +1,6 @@
 #!/usr/bin/env ruby
 ##########################################
-#         PHP/Base64 Encoder 1.0         #
+#         PHP/Base64 Encoder 1.1         #
 # A script that will encode any binary   #
 # file and output a self-extracting php. #
 ##########################################
@@ -8,7 +8,11 @@
 require 'base64'
 require 'optparse'
 
-options = {:output => false}
+options = {
+:output => false,
+:execute => false,
+:verbose => false
+}
 
 optparse = OptionParser.new do|opts|
   script_name = File.basename($0)
@@ -25,6 +29,17 @@ optparse = OptionParser.new do|opts|
   
   opts.on('-o filename', '--output filename', String, 'Path to output file') do|f|
     options[:output] = f
+    puts "[*] Output is set to: #{f}"
+  end
+  
+  opts.on('-x', '--execute', 'Auto execute extracted file') do
+    options[:execute] = true
+    puts "[*] Auto execution after extraction set to on."
+  end
+  
+  opts.on('-v', '--verbose', 'PHP script in verbose mode') do
+    options[:verbose] = true
+    puts "[*] Verbose mode set to on."
   end
 end
 
@@ -49,7 +64,12 @@ encoded = "<?php $i=base64_decode(\""
 enc.each_line {|l|
   encoded += l.chomp()
 }
-encoded += "\");file_put_contents(\"#{name}\",$i); ?>"
+encoded += "\");$a=file_put_contents(\"#{name}\",$i);"
+
+encoded += "if ($a) echo 'file written successfully. '; else echo 'error writing file. ';" if options[:verbose]
+encoded += "if(PHP_OS == 'WINNT' || PHP_OS == 'WIN32') pclose(popen('start /b #{name}', 'r')); else pclose(popen(#{name}.' >> /dev/null &', 'r'));" if options[:execute]
+encoded += "echo 'file executed.';" if options[:verbose] and options[:execute]
+encoded+= " ?>"
 
 #if :output is set just write out the encoded file
 if options[:output] then
